af5ef94d8ab73c13cff4bf1bb43aa4ff3bb1ba8d
PrashantChoudhary184 <choudharyprashant350@gmail.com>
2023-09-19 19:46:03 +0530

    Commit

---
+++ msbaexpensetracker/src/test/java/com/prashantexpense/msbaexpensetracker/MsbaexpensetrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prashantexpense.msbaexpensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsbaexpensetrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
